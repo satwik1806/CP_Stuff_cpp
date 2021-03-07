28c3dbac13687d6ce3ae9040f731c062fb9b1eb3
satwik1806 <53432706+satwik1806@users.noreply.github.com>
2021-03-07 18:19:37 +0530

    Update prime_factorization.cpp

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -3,7 +3,7 @@ int spf[mxn];
 void precompute() {
     for (int i = 0; i < mxn; i++) spf[i] = 1;
 
-    for (int i = 2; i < mxnli++) {
+    for (int i = 2; i < mxn;i++) {
         if (spf[i] == 1) {
             spf[i] = i;
             for (int j = 2 * i; j < mxn; j += i)
