78827879a5c41c4d0c599f12786329745f953840
satwik1806 <53432706+satwik1806@users.noreply.github.com>
2021-03-07 21:52:16 +0530

    updated fenwick tree

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,17 +1,57 @@
+// Fenwick tree. kindaa dynamic prefix sum
+// author - Satwik_Tiwari
+int a[mxn];
 template <typename T>
 class BinaryIndexedTree {
 private:
-	vector<T> data;
+    vector<T> data;
 public:
-	BinaryIndexedTree(int n = 0) : data(n + 1) { }
-	T sum(int i) {
-		T s = T();
-		for (; i > 0; i -= i & -i) { s += data[i]; }
-		return s;
-	}
-	void add(int i, const T &x) {
-		for (++i; i < static_cast<int>(data.size()); i += i & -i) {
-			data[i] += x;
-		}
-	}
+    BinaryIndexedTree(int n) : data(n) {
+        for (int i = 0; i < n; i++) data[i] = a[i];
+        for (int i = 0; i < n; i++) {
+            int j = i | (i + 1);
+            if (j < n)
+                data[j] += data[i];
+        }
+    }
+
+    T sum(int i) { // ask queries like [0,i)
+        T s = T();
+        s = 0;
+        while (i) {
+            s += data[i - 1];
+            i &= i - 1;
+        }
+        return s;
+    }
+
+    void update(int i, const T &x) { // bit[i] += x
+        while (i < size(data)) {
+            data[i] += x;
+            i |= i + 1;
+        }
+    }
+
+    //for binary searching in fenwick tree.
+    int findkth(int k) { // find largest index such that the sum(idx) <= k
+        int ind = -1; int x = size(data);
+        int bits;
+        for (bits = 0; x != 0; ++ bits) x >>= 1;
+        for (int d = bits - 1; d >= 0; d--) {
+            int right_ind = ind + (1 << d);
+            if (right_ind < size(data) && k >= data[right_ind]) {
+                ind = right_ind;
+                k -= data[ind];
+            }
+        }
+        return ind; // return -1 if k < sum(1).
+    }
+
+    void printpref() { //for debugging incase. don't forget to commentout.
+        vector<int> out;
+        for (int i = 1; i <= size(data); i++) out.push_back(sum(i));
+        debug(out);
+    }
 };
+
+//update array a in main.
