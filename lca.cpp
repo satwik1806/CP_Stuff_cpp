29b005dedf723d1e2cc477578a2c65cbfcb70c65
satwik1806 <53432706+satwik1806@users.noreply.github.com>
2021-05-10 22:18:40 +0530

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -3,41 +3,42 @@
 // whole graph is 0 indexed and queries as well.
 // graph is global as well.
 
-int aa[mxn];
+//Range Queries.
+// Author - Satwik_Tiwari
 template<typename T>
-class RangeQuery {
+class RangeQuery{
 private:
     vector<vector<T>> data;
 public:
-    void RangeQuery_precompute(int n) {
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            temp.push_back(aa[i]);
+    void precal(vector<T> &arr){ // make sure not to pass a global vector of size = mxn.
+        vector<int> temp; int n = arr.size();
+        for(int i=0;i<n;i++){
+            temp.push_back(arr[i]);
         }
         data.push_back(temp);
         int i = 1;
-        while (2 * i <= n) {
-            vector<int> prev = data[size(data) - 1];
+        while(2*i <= n){
+            vector<int> prev = data[size(data)-1];
             vector<int> put;
-            for (int j = 0; j < n - 2 * i + 1; j++) {
-                put.push_back(func(prev[j], prev[j + i]));
+            for(int j=0;j<n-2*i+1;j++){
+                put.push_back(func(prev[j],prev[j + i]));
             }
             data.push_back(put);
             i <<= 1;
         }
     }
 
-    T func(T aaa, T ab) { // change it according to you.
-        return min(aaa, ab);
+    T func(T aa, T ab){ // change it according to you.
+        return min(aa,ab);
     }
 
-    T query(int st, int end) { // ask queries like query[st,end).
-        int bits; int x = end - st;
-        for (bits = 0; x != 0; ++ bits) x >>= 1; bits--;
-        return (func(data[bits][st], data[bits][end - (1 << bits)]));
+    T query(int st,int end){ // ask queries like query[st,end].
+        int bits; end++; int x = end - st;
+        for(bits = 0;x != 0; ++ bits) x >>= 1; bits--;
+        return (func(data[bits][st],data[bits][end - (1 << bits)]));
     }
 
-    T getitem(int idx) {
+    T getitem(int idx){
         return (data[0][idx]);
     }
 };
@@ -45,11 +46,11 @@ public:
 template<typename T>
 class LCA {
 private:
-    vector<T> time;
-    vector<T> path;
-    RangeQuery<T> rmq;
+    vector<int> time;
+    vector<int> path;
+    RangeQuery<int> rmq;
 public:
-    LCA(T root, int n) {
+    LCA(int root, int n) {
         vector<int> P;
         for (int i = 0; i < n; i++) {
             time.push_back(-1); path.push_back(-1); P.push_back(-1);
@@ -66,12 +67,12 @@ public:
                 }
             }
         }
-        int ii = 0 ;
-        for (int i : path) aa[ii++] = time[i == -1 ? n - 1 : i];
-        rmq.RangeQuery_precompute(n);
+        vector<int> aa;
+        for (int i : path) aa.push_back(time[i == -1 ? n - 1 : i]);
+        rmq.precal(aa);
     }
 
-    T query(int a, int b) {
+    int query(int a, int b) {
         if (a == b) return a;
         a = time[a]; b = time[b];
         if (a > b) swap(a, b);
